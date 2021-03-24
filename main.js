f4bad2b7d1e9772151bb8f896c553c91362f0dff
renattosilvacorretor <73900623+renattosilvacorretor@users.noreply.github.com>
2021-03-24 14:46:59 -0300

    load completo

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 function enviaContato(nome, numero) {
+	let load = document.getElementById('load');
+	let sucesso = document.getElementById('sucesso');
+	let falha = document.getElementById('falha');
+	load.style.display = 'none';
+	sucesso.style.display = 'none';
+	falha.style.display = 'none';
+	load.style.display = 'flex';
+
 	axios.post('https://contato-api-rps.herokuapp.com/', {
 		nome,
 		numero
 	  })
 	  .then(function (response) {
-	    console.log(response);
-	    alert('Cadastrado com sucesso!')
-	    document.getElementById('contactName').value='';
-	    document.getElementById('contactNum').value='';
+		console.log(response);
+		load.style.display = 'none';
+		sucesso.style.display = 'block';
+		falha.style.display = 'none';
+		document.getElementById('contactName').value='';
+		document.getElementById('contactNum').value='';
 	  })
 	  .catch(function (error) {
+		load.style.display = 'none';
+		sucesso.style.display = 'none';
+		falha.style.display = 'block';
+		document.getElementById('contactName').value='';
+		document.getElementById('contactNum').value='';
 	    console.log(error);
 	  });
 }
 
-const btn = document.getElementById('enviaContatobt');
-btn.onclick = async function() {
-	const imputNome = document.getElementById('contactName');
-	const imputNum = document.getElementById('contactNum');
+let formCadastro = document.getElementById('formcadastro');
+
+formCadastro.addEventListener('submit', async function(event) {
+	event.preventDefault();
+
+	let imputNome = document.getElementById('contactName');
+	let imputNum = document.getElementById('contactNum');
 	
 	await enviaContato(imputNome.value, imputNum.value)
-}
+})
